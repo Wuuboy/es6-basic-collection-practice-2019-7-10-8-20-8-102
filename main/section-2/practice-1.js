fc700f2e9e9a93a3bf3d223288d26ff73f0614c0
曹桂凤 <demi.cao@oocl.com>
2019-07-12 09:10:27 +0800

    feat:implement js-coolection sesion2 practice-1

--- main/section-2/practice-1.js
+++ main/section-2/practice-1.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-
+    let resultArray =[]
+    let inputObject = {}
+    collection.filter(x => (inputObject[x]=(inputObject[x]+1)||1))
+    let keys = Object.keys(inputObject)
+    keys.filter(x =>{
+        let resultObject = {
+            key:x,
+            count:inputObject[x]
+        }
+        resultArray.push(resultObject)
+    }
+   )
+    return resultArray
 }
