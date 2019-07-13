55fa64d271f1a9171407ec91a49798924aa04edf
曹曹 <1992942606@qq.com>
2019-07-13 11:13:47 +0800

    [Demi]feat:js-collection implement setion2 practice3

--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  //decode the input to {word:count}
+  let inputObject = {}
+  collection.filter(word =>{
+    if(word.length==1){
+      inputObject[word]=inputObject[word]+1||1
+    }else {
+      const count = parseInt(word.replace(/[^0-9]/ig,""));
+      const decodedWord = word.substring(0,1)
+      inputObject[decodedWord] = inputObject[decodedWord]?inputObject[decodedWord]+count:count
+    }
+  })
+  // store result [{word:count}]
+  let result = []
+  Object.keys(inputObject).filter(word =>{
+    const keyToCount = {
+      name:word,
+      summary:inputObject[word]
+    }
+    result.push(keyToCount)
+  })
+  return result;
 }
